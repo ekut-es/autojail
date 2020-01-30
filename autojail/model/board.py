3f2ab12c5f8ced038c2312a9524a7a3c35b5c4b9
Dennis Volm <dennis.volm@student.uni-tuebingen.de>
2020-01-30 14:58:42 +0100

    Add example for board definitions

---
+++ autojail/model/board.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from typing import Dict, List
+
+class MemoryRegion(BaseModel):
+    physical_start_addr: int
+    virtual_start_addr: int
+    size: int
+    flags: List[str] #Besser ENUM
+
+class Board(BaseModel):
+    id: str
+    name: str
+    board: str
+
+    memory_regions: Dict[str, MemoryRegion]
+
+
+if __name__ == "__main__":
+    import yaml
+    import sys
+    from pprint import pprint
+
+    with open(sys.argv[1]) as yaml_file:
+        yaml_dict = yaml.load(yaml_file)
+        pprint(yaml_dict, indent=2)
+
+        board = Board(**yaml_dict)
+        pprint(board, indent=2)
