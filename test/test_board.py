6eac8fbfca08af90aa94276ac3eaf57bde92f6ed
Dennis Volm <dennis.volm@student.uni-tuebingen.de>
2020-02-03 12:24:24 +0100

    Add test for yaml parsing of board data

--- test/test_board.py
+++ test/test_board.py
@@ -1,4 +1,10 @@
+import os.path
+import yaml
+
 from autojail.model.board import Board
+
+
+test_data_folder = os.path.join(os.path.dirname(__file__), "test_data")
 
 
 def test_board():
@@ -27,3 +33,11 @@ def test_board():
     )
     assert board_model.memory_regions["test_region"].size == 100
 
+
+def test_board_from_yaml():
+    board_yaml = os.path.join(test_data_folder, "test_board.yml")
+    with open(board_yaml) as board_yaml_file:
+        board_dict = yaml.load(board_yaml_file)
+        board_model = Board(**board_dict)
+
+        assert board_model.id == "board1"
