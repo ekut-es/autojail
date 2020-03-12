3d1addf7181f462552b0f511cbf8035fa33b530d
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2020-03-12 13:57:07 +0100

    Check for virtual environment

--- scripts/install_dtc.sh
+++ scripts/install_dtc.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ "$VIRTUAL_ENV" == "" ]]; then
+    echo "Could not detect virtual environment"
+    echo "Please run 'poetry shell' first"
+    exit -1 
+fi
 
 if [ ! -d dtc ] ; then
     git clone git://git.kernel.org/pub/scm/utils/dtc/dtc.git
