b3a956d90d33a84b1ea9fde2972c3a6b2a34216c
Christoph Gerum <christoph@gerum.de>
2020-06-30 10:28:46 +0200

    Add golden reference

---
+++ projects/rpi4_two_net_inmates/golden/linux-demo-2.c
@@ -0,0 +1,109 @@
+#include <jailhouse/types.h>
+#include <jailhouse/cell-config.h>
+struct { 
+	struct jailhouse_cell_desc cell; 
+	__u64 cpus[1];
+	struct jailhouse_memory mem_regions[9];
+	struct jailhouse_irqchip irqchips[2];
+	struct jailhouse_pci_device pci_devices[1];
+} __attribute__((packed)) config = {
+
+.cell = {
+	.signature = JAILHOUSE_CELL_DESC_SIGNATURE,
+	.revision = JAILHOUSE_CONFIG_REVISION,
+	.flags = JAILHOUSE_CELL_PASSIVE_COMMREG | JAILHOUSE_CELL_VIRTUAL_CONSOLE_PERMITTED,
+	.console = {
+		.address = 0xfe215040,
+		.size = 0x40,
+		.type = JAILHOUSE_CON_TYPE_8250,
+		.flags = JAILHOUSE_CON_ACCESS_MMIO | JAILHOUSE_CON_REGDIST_4,
+	},
+
+		.name = "Linux demo 2" ,
+		.vpci_irq_base = 153,
+		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
+		.num_pci_devices = ARRAY_SIZE(config.pci_devices),
+		.cpu_set_size = sizeof(config.cpus),
+		.num_irqchips = ARRAY_SIZE(config.irqchips),
+	},
+	.cpus = {0b1000},
+	
+	.mem_regions = {
+	/*RAM 2.1 0xf3fd9000-0xf3fe9000*/	{
+		.phys_start = 0xf3fd9000,
+		.virt_start = 0x0,
+		.size = 0x10000,
+		.flags = JAILHOUSE_MEM_READ|JAILHOUSE_MEM_WRITE|JAILHOUSE_MEM_EXECUTE|JAILHOUSE_MEM_LOADABLE|JAILHOUSE_MEM_ROOTSHARED,
+	},
+	/*fe215040.serial 0xfe215040-0xfe215080*/	{
+		.phys_start = 0xfe215040,
+		.virt_start = 0xfe215040,
+		.size = 0x40,
+		.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE | JAILHOUSE_MEM_IO | JAILHOUSE_MEM_IO_8 | JAILHOUSE_MEM_IO_16 | JAILHOUSE_MEM_IO_32 | JAILHOUSE_MEM_IO_64 | JAILHOUSE_MEM_ROOTSHARED,
+	},
+	/*fe200000.gpio 0xfe200000-0xfe2000b4*/	{
+		.phys_start = 0xfe200000,
+		.virt_start = 0xfe200000,
+		.size = 0xb4,
+		.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE | JAILHOUSE_MEM_IO | JAILHOUSE_MEM_IO_8 | JAILHOUSE_MEM_IO_16 | JAILHOUSE_MEM_IO_32 | JAILHOUSE_MEM_IO_64 | JAILHOUSE_MEM_ROOTSHARED,
+	},
+	/*RAM 2.2 0xebfd9000-0xf3fd9000*/	{
+		.phys_start = 0xebfd9000,
+		.virt_start = 0x40000000,
+		.size = 0x8000000,
+		.flags = JAILHOUSE_MEM_READ|JAILHOUSE_MEM_WRITE|JAILHOUSE_MEM_EXECUTE|JAILHOUSE_MEM_DMA|JAILHOUSE_MEM_LOADABLE|JAILHOUSE_MEM_ROOTSHARED,
+	},
+	/*communication_region 0xf3fe9000-0xf3fea000*/	{
+		.phys_start = 0xf3fe9000,
+		.virt_start = 0x80000000,
+		.size = 0x1000,
+		.flags = JAILHOUSE_MEM_READ|JAILHOUSE_MEM_WRITE|JAILHOUSE_MEM_COMM_REGION,
+	},
+	/*net2_0 0xfbffa000-0xfbffb000*/	{
+		.phys_start = 0xfbffa000,
+		.virt_start = 0xfbffa000,
+		.size = 0x1000,
+		.flags = JAILHOUSE_MEM_READ|JAILHOUSE_MEM_ROOTSHARED,
+	},
+	/* empty optional region */ { 0 },
+	/*net2_2 0xfbffb000-0xfbffc000*/	{
+		.phys_start = 0xfbffb000,
+		.virt_start = 0xfbffb000,
+		.size = 0x1000,
+		.flags = JAILHOUSE_MEM_READ|JAILHOUSE_MEM_ROOTSHARED,
+	},
+	/*net2_3 0xfbffc000-0xfbffd000*/	{
+		.phys_start = 0xfbffc000,
+		.virt_start = 0xfbffc000,
+		.size = 0x1000,
+		.flags = JAILHOUSE_MEM_READ|JAILHOUSE_MEM_ROOTSHARED|JAILHOUSE_MEM_WRITE,
+	},
+	},
+	.irqchips = {
+		{
+			.address = 0xff841000,
+			.pin_base = 32,
+			.pin_bitmap = {},
+		},
+		{
+			.address = 0xff841000,
+			.pin_base = 160,
+			.pin_bitmap = {0x4000000},
+		},
+	},
+
+	.pci_devices = {
+		/*net2*/
+		{
+			.type = JAILHOUSE_PCI_TYPE_IVSHMEM,
+			.domain = 1,
+			.bar_mask = JAILHOUSE_IVSHMEM_BAR_MASK_INTX,
+			.bdf = 1 << 3,
+			.shmem_regions_start = 5,
+			.shmem_dev_id = 1,
+			.shmem_peers = 2,
+			.shmem_protocol = JAILHOUSE_SHMEM_PROTO_VETH,
+		},
+	},
+
+};
