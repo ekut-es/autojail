9db04387ed9e616e23e337a563296d08ad48ea7d
Joscha Benz <jbenz@mailbox.org>
2020-07-16 14:43:00 +0200

    Fix: IRQ chip bitmap generation

--- autojail/config/irq.py
+++ autojail/config/irq.py
@@ -20,7 +20,7 @@ class PrepareIRQChipsPass(BasePass):
     def _prepare_irqchips(self, cell):
         "Splits irqchips that handle more interrupts than are possible in one autojail config entry"
 
-        split_factor = 32 * 4  # One entry can handle only  4*32 interrupts
+        split_factor = 32 * 5  # One entry can handle only  4*32 interrupts
         new_irqchips = {}
         for name, irqchip in cell.irqchips.items():
             count = 0
@@ -32,6 +32,7 @@ class PrepareIRQChipsPass(BasePass):
             )
 
             current_base = 0
+
             for irq in sorted(irqchip.interrupts):
                 while irq >= current_base + split_factor:
                     new_irqchips[new_name] = new_chip
@@ -39,14 +40,17 @@ class PrepareIRQChipsPass(BasePass):
                     current_base += split_factor
                     new_chip = IRQChip(
                         address=irqchip.address,
-                        pin_base=irqchip.pin_base + current_base,
+                        pin_base=current_base,
                         interrupts=[],
                     )
 
                     count += 1
                     new_name = name + "_" + str(count)
 
-                new_chip.interrupts.append(irq - current_base)
+                # each chip has four bases: 0 <= i <= 3: current_base + (i*32)
+                # since each chip has a bitmap of 4*32 bit integers
+                current_base_offset = int((irq - current_base) / 32) * 32
+                new_chip.interrupts.append(irq - (current_base + current_base_offset))
 
             new_irqchips[new_name] = new_chip
 
