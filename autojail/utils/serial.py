9d9a8afc25fc0e2f1df7434b5855118809e6a32f
Christoph Gerum <christoph@gerum.de>
2020-08-06 11:24:22 +0200

    Add serial connector

---
+++ autojail/utils/serial.py
@@ -0,0 +1,18 @@
+import io
+import socket
+from typing import Optional, Union
+from urllib.parse import urlparse
+
+import serial
+
+
+def open_serial(
+    url: str, encoding: Optional[str] = "utf-8"
+) -> Union[io.RawIOBase, serial.Serial]:
+    parsed_url = urlparse(url)
+    if parsed_url.scheme == "unix":
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sock.connect(parsed_url.path)
+        return sock.makefile("rw", encoding=encoding)
+    else:
+        return serial.serial_for_url(url, encoding=encoding)
