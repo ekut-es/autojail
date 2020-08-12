5aa4299bc4851cc62f9d68572e8e859b1537b096
Christoph Gerum <gerum@informatik.uni-tuebingen.de>
2020-08-12 12:16:52 +0200

    Update fs.py

--- autojail/utils/fs.py
+++ autojail/utils/fs.py
@@ -1,8 +1,12 @@
 import os
 
-def which(pgm):
+from typing import Union
+
+def which(pgm : str) -> Union[str, None]:
     path=os.getenv('PATH')
     for p in path.split(os.path.pathsep):
         p=os.path.join(p,pgm)
         if os.path.exists(p) and os.access(p,os.X_OK):
             return p
+            
+    return None
