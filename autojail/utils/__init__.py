4aa5f452949326c6f86a2da29c650495ab1afbb0
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2020-08-12 13:28:05 +0200

    Fix sorting of imports

--- autojail/utils/__init__.py
+++ autojail/utils/__init__.py
@@ -1,10 +1,10 @@
 from .collections import SortedCollection
 from .connection import connect  # noqa
 from .debug import debug
+from .fs import which
 from .intervall_arithmetic import get_overlap
 from .logging import ClikitLoggingHandler
 from .string import pprint_tree, remove_prefix
-from .fs import which
 
 __all__ = [
     "connect",
