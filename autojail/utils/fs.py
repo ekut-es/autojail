328719b617cc80dca88122dbffa0d3a734269957
Christoph Gerum <gerum@informatik.uni-tuebingen.de>
2020-08-12 12:11:08 +0200

    Add which() helper to locate executables.

---
+++ autojail/utils/fs.py
@@ -0,0 +1,8 @@
+import os
+
+def which(pgm):
+    path=os.getenv('PATH')
+    for p in path.split(os.path.pathsep):
+        p=os.path.join(p,pgm)
+        if os.path.exists(p) and os.access(p,os.X_OK):
+            return p
